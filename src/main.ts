e81c446077403bc70bb910d8cb6ee14b6281a0ab
Филимонов Сергей <fsb2603@mail.ru>
2025-03-05 17:36:01 +0700

    feature: add element plus

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
+
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ElementPlus)
 
 app.mount('#app')
